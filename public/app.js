794d402239ca2142def158c41d5db28441a3ff0b
tofikhidayat <topikhidayat711@gmail.com>
2020-09-06 15:12:12 +0700

    feat: update

--- public/app.js
+++ public/app.js
@@ -12,7 +12,7 @@ socket.on('connect', () => {
     console.log(msg)
   })
   socket.on('update', (update) => {
-    console.log(oldValue)
+    console.log(update)
     oldValue = {...oldValue, update}
     root.textContent = JSON.stringify(oldValue)
   })
